6e610c661f8dbaa1b99dbf72a64d7470dfe02016
Arturo Avalos <137134833+oruava@users.noreply.github.com>
2023-10-04 00:50:51 -0300

    Add files via upload

---
+++ Calendario/TestFecha.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class TestFecha {
+    public static void main(String[] args) {
+        TodaysDate hoy = new TodaysDate();
+        hoy.printDateAndTime();
+    }
+}
+
+
+
+
